364c0f20e1361af4c9be0ecfd56225f353552106
KimDoHyun <kdhqwe1030@naver.com>
2024-12-13 20:47:08 +0900

    feat: 동아리 수정/삭제 api 구현

--- src/routes/club.js
+++ src/routes/club.js
@@ -6,4 +6,7 @@ const clubController = require('../controllers/club');
 router.post('/clubSearch', clubController.searchClub);
 router.post('/clubMembers', clubController.searchClubMembers);
 router.post('/clubAdvisor', clubController.searchClubAdvisor);
+router.post('/clubModify', clubController.modifyClub);
+router.post('/clubDelete', clubController.deleteClub);
+
 module.exports = router;
